c34b211fbece793ba4f66740748ced1bb9e2c50a
rkmiller131 <rkmiller131@gmail.com>
2025-02-17 09:26:14 -0800

    Updated position of sphere

--- src/3d/PlanetSphere.tsx
+++ src/3d/PlanetSphere.tsx
@@ -2,7 +2,7 @@ import { fragmentShader, vertexShader } from "./shaders/planetSphere.shaders";
 
 export default function PlanetSphere () {
   return (
-    <mesh>
+    <mesh position={[0, 0.5, 0]}>
       <icosahedronGeometry args={[1.75, 10]} />
       <shaderMaterial
         vertexShader={vertexShader}
